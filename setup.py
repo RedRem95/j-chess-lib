6ad038c3bb22a6455a2307d5be372e4265a66382
RedRem95 <alexander.vollmer.95@hotmail.de>
2022-01-25 20:34:51 +0100

    schema install is now a pre step in build_py, install_lib, sdist, bdist, upload, bdist_wheel This should cover every install method. Kinda hacky maybe has to be revisited

--- setup.py
+++ setup.py
@@ -2,36 +2,42 @@
 
 """The setup script."""
 import os
+from distutils.command import build_py, install_lib, sdist, bdist, upload
 from distutils.dist import Distribution
-from typing import Any
+from typing import Any, Type
 
-from setuptools import Command
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
 
 schema_package = os.environ.get("j-chess-schema-package", "j_chess_lib.communication.schema")
 os.makedirs(schema_package.replace(".", "/"), exist_ok=True)
 
+_installed_schema = [False]
+
 
 def install_schema_package():
-    print("-" * 10, "schema install", "-" * 10)
-    xsd_url = os.environ.get("j-chess-xsd-url",
-                             "https://raw.githubusercontent.com/JoKrus/j-chess-xsd/master/jChessMessage.xsd")
-    import subprocess
-    try:
-        subprocess.run(["rm", "-rf", "j_chess_lib/communication/schema"], check=True)
-        print("Removed old schema data")
-    except subprocess.CalledProcessError:
-        pass
-    print("Load schema data from %s and install them" % xsd_url)
-    subprocess.run(["xsdata", xsd_url, "--package", schema_package], check=True)
-    print("Installed new schema data")
-    print("-" * 10, "schema install", "-" * 10)
-
-
-install_schema_package()
+    if not _installed_schema[0]:
+        print("-" * 10, "schema install", "-" * 10)
+        xsd_url = os.environ.get("j-chess-xsd-url",
+                                 "https://raw.githubusercontent.com/JoKrus/j-chess-xsd/master/jChessMessage.xsd")
+        import subprocess
+        try:
+            subprocess.run(["rm", "-rf", "j_chess_lib/communication/schema"], check=True)
+            print("Removed old schema data")
+        except subprocess.CalledProcessError:
+            pass
+        print("Load schema data from %s and install them" % xsd_url)
+        subprocess.run(["xsdata", xsd_url, "--package", schema_package], check=True)
+        print("Installed new schema data")
+        print("-" * 10, "schema install", "-" * 10)
+    else:
+        print("-" * 10, "schema already installed", "-" * 10)
+    _installed_schema[0] = True
 
 
 class InstallSchema(Command):
+    key = "install_schema"
+    description = "Create classes from the j-chess schema"
+    user_options = []
 
     def __init__(self, dist: Distribution, **kw: Any):
         super().__init__(dist, **kw)
@@ -44,8 +50,33 @@ class InstallSchema(Command):
 
     def run(self):
         install_schema_package()
-        super(InstallSchema, self).run()
 
+
+def install_schema_wrapper(class_type) -> Type[Command]:
+    class Wrapper(class_type):
+
+        def run(self) -> None:
+            install_schema_package()
+            super(Wrapper, self).run()
+
+    return Wrapper
+
+
+cmd_classes = {
+    InstallSchema.key: InstallSchema,
+    "build_py": install_schema_wrapper(build_py.build_py),
+    "install_lib": install_schema_wrapper(install_lib.install_lib),
+    "sdist": install_schema_wrapper(sdist.sdist),
+    "bdist": install_schema_wrapper(bdist.bdist),
+    "upload": install_schema_wrapper(upload.upload),
+}
+
+try:
+    from wheel.bdist_wheel import bdist_wheel
+
+    cmd_classes["bdist_wheel"] = install_schema_wrapper(bdist_wheel)
+except ImportError:
+    pass
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -84,5 +115,5 @@ setup(
     url='https://github.com/RedRem95/j_chess_lib',
     version='0.2.2',
     zip_safe=False,
-    # cmdclass={},
+    cmdclass=cmd_classes,
 )
