49755cf07518c8f45eaa268015c5c611191ae583
RedRem95 <alexander.vollmer.95@hotmail.de>
2022-02-02 01:30:31 +0100

    Added metrics for AIs These do not have to be overwritten or extended. Btut nice to have if you want to check on your AI during playtime

--- j_chess_lib/ai/ai.py
+++ j_chess_lib/ai/ai.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Tuple, Dict
+from typing import Optional, Tuple, Dict, Any, List
 from uuid import UUID
 
+from .container import GameState
 from ..communication.schema import MatchStatusData, MatchFormatData, MoveData
-from .Container import GameState
 
 
 class AI(ABC):
@@ -38,6 +38,10 @@ class AI(ABC):
     def get_move(self, game_id: UUID, match_id: UUID, game_state: GameState) -> MoveData:
         pass
 
+    # noinspection PyMethodMayBeStatic
+    def metrics(self) -> List[Tuple[str, Any]]:
+        return [("name", self.name)]
+
 
 class DumbAI(AI, ABC):
 
@@ -60,6 +64,8 @@ class StoreAI(AI, ABC):
         super().__init__(name)
         self._match_storage: Dict[UUID, Tuple[str, MatchFormatData]] = {}
         self._game_storage: Dict[Tuple[UUID, UUID], str] = {}
+        self._current_match: Optional[UUID] = None
+        self._current_game: Optional[Tuple[UUID, UUID]] = None
 
     def get_match(self, match_id: UUID) -> Optional[Tuple[str, MatchFormatData]]:
         return self._match_storage.get(match_id, None)
@@ -69,12 +75,32 @@ class StoreAI(AI, ABC):
 
     def new_match(self, match_id: UUID, enemy: str, match_format: MatchFormatData):
         self._match_storage[match_id] = (enemy, match_format)
+        self._current_match = match_id
 
     def finalize_match(self, match_id: UUID, status: MatchStatusData, statistics: str):
         pass
 
     def new_game(self, game_id: UUID, match_id: UUID, white_player: str):
         self._game_storage[(match_id, game_id)] = white_player
+        self._current_game = (game_id, match_id)
 
     def finalize_game(self, game_id: UUID, match_id: UUID, winner: Optional[str], pgn: str):
         pass
+
+    def metrics(self) -> List[Tuple[str, Any]]:
+        metrics = super().metrics()
+        if self._current_match is not None:
+            enemy, match = self.get_match(match_id=self._current_match)
+            match: MatchFormatData
+            metrics.append(("In match", self._current_match))
+            metrics.append(("   against", enemy))
+            metrics.append(("   match type", f"{match.match_type_data}"))
+        if self._current_game is not None:
+            game_id, match_id = self._current_game
+            enemy, match = self.get_match(match_id=match_id)
+            white_player = self.get_game(game_id=game_id, match_id=match_id)
+            metrics.append(("In game", game_id))
+            metrics.append(("   of match", f"{match_id} -> {match.match_type_value.value}"))
+            metrics.append(("   against", enemy))
+            metrics.append(("   white player", f"{white_player} <- {'me' if white_player == self.name else 'enemy'}"))
+        return metrics
