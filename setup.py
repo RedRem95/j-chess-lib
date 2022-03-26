db78733b6c22c4c34f432bfbe55082e3a3bb9920
RedRem95 <redrem_github@botschmot.de>
2022-03-26 21:03:57 +0100

    Bump version: 0.9.2 → 0.10.0

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/RedRem95/j_chess_lib',
-    version='0.9.2',
+    version='0.10.0',
     zip_safe=True,
     cmdclass=cmd_classes,
 )
